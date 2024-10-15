eb3ce4df6580310a8ee0a17976a37c4c015b5ef1
lyhduc <lyhongduc.34@gmail.com>
2024-10-15 19:52:38 +0700

    Test commit

--- server/config/config.js
+++ server/config/config.js
@@ -1,9 +1,11 @@
+require('dotenv').config();
+
 module.exports = {
-    HOST: "localhost",
-    USER: "root",
-    PASSWORD: "lyhongduc123",
-    DB: "salon",
-    dialect: "mysql",
+    HOST: process.env.DB_HOST,
+    USER: process.env.DB_USER,
+    PASSWORD: process.env.DB_PASS,
+    DB: process.env.DB_NAME,
+    dialect: process.env.DB_DIALECT,
     pool: {
         max: 10,
         min: 0,
